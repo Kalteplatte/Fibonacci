14546b28e9ad58e70034246604dc9b4b3e47dc00
Kalteplatte <david.steffen@uni-jena.de>
2014-11-02 22:49:42 +0100

    worked on 2. function

--- Fibonacci.c
+++ Fibonacci.c
@@ -2,29 +2,24 @@
 #include <math.h>
 #include <iostream>
 
-int Fibonacci1 (int n){
-	if (n<0){
-		return n;
-	}
-	if (n==0){ 
-		return n;
-	}
-	if (n==1){
+int Fibonacci1 (int n){  //recursive function
+	if (n<=1){
 		return n;
 	}
 	return Fibonacci1(n-1)+Fibonacci1(n-2);
 
 }
 
-int Fibonacci2 (int n){
-	if (n<0){
+int Fibonacci2 (int n){  //function with linear time and memory
+	if(n<=1){
 		return n;
 	}
-	if (n==0|n==1){ 
-		return n;
+	fib[0]=0;
+	fib[1]=1;
+	for (int i=2; i==n-1; i++){
+		fib[i]=fib[i-1]+fib[i-2];
 	}
-	return Fibonacci1(n-1)+Fibonacci1(n-2);
-
+	return fib[n-1];
 }
 
 int main (){
